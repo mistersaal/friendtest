06575ca3852f3dd04af5d4b88c055ecd1b90e19e
Сааль Степан <mister.saal@yandex.ru>
2020-08-15 22:26:27 +0300

    Изменил место подписки

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -100,6 +100,13 @@
                 this.$router.push(this.$route.fullPath + '#delete')
             },
             createTest() {
+                bridge.send("VKWebAppJoinGroup", {group_id: this.$store.state.group})
+                    .then(() => {
+                        bridge.send("VKWebAppAllowMessagesFromGroup", {group_id: this.$store.state.group})
+                    })
+                    .catch(() => {
+                        bridge.send("VKWebAppAllowMessagesFromGroup", {group_id: this.$store.state.group})
+                    })
                 this.$router.push('/create')
             },
             share() {
