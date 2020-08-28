88434a8cb51a576a64b88abb8e72dad5fdaf5cb7
Сааль Степан <mister.saal@yandex.ru>
2020-08-28 21:26:09 +0300

    Добавил приложение в список и предсказания

--- resources/js/data/apps.js
+++ resources/js/data/apps.js
@@ -5,6 +5,11 @@ export default [
         image: 'https://sun9-49.userapi.com/c854220/v854220495/24f9de/9xpArTDQBoo.jpg'
     },
     {
+        name: 'Сексуальность',
+        link: 'https://vk.com/app7571253',
+        image: 'https://sun1-98.userapi.com/8_DL8UJzKVU-L06W46ITct72ahT0fRau5NIVWw/X8dZ0rhdn-4.jpg'
+    },
+    {
         name: 'Вторая половинка',
         link: 'https://vk.com/app7570885',
         image: 'https://sun9-52.userapi.com/aYHjmgOJYtpmV1P9eRK-JDm89bdwpsPNaEQ-_g/E3iGvPpZjwI.jpg'
